9f84c6f1e8ec63bd5569c081a04584bee92aa71e
Romain Minot <rom_ino@hotmail.fr>
2023-10-14 01:13:57 +0200

    Page transition start and button component

---
+++ src/scripts/utils.ts
@@ -0,0 +1,14 @@
+/**
+ * @param stringToSeparate
+ * @returns 
+ */
+export function separateCharactersFromString(stringToSeparate: string): string{
+    let separateString = "";
+    const newString = stringToSeparate.split("");
+
+    for (let i = 0; i < newString.length; i++) {
+        separateString += "<span>" + stringToSeparate[i] + "</span>"
+    }   
+
+    return separateString;
+}
